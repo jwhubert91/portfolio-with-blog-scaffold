45f1fd61add1885d0b5cd9dfe64bf8e500ee7fa1
james w hubert <jwhubert91@gmail.com>
2019-11-16 16:50:27 -0500

    used a resource generator to create a resource called 'Works', which was used for a migration, controller, and new views directory. Singular version was used for model.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_16_203131) do
+ActiveRecord::Schema.define(version: 2019_11_16_213948) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,16 @@ ActiveRecord::Schema.define(version: 2019_11_16_203131) do
   create_table "skills", force: :cascade do |t|
     t.string "title"
     t.integer "percent_used"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "works", force: :cascade do |t|
+    t.string "title"
+    t.string "subtitle"
+    t.text "body"
+    t.text "main_img"
+    t.text "thumb_img"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
