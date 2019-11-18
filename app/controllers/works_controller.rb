14d9494bd8fa8206c6452bdb740910fb2bdf9e2e
james w hubert <jwhubert91@gmail.com>
2019-11-18 12:42:41 -0500

    Integrated new and create actions for Works controller with views

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,2 +1,23 @@
 class WorksController < ApplicationController
+
+	def index
+		@portfolio_items = Work.all
+	end
+
+	def new
+		@portfolio_item = Work.new
+	end
+
+	def create
+    @portfolio_item = Work.new(params.require(:work).permit(:title, :subtitle, :body))
+
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to works_path, notice: 'Your portfolio item is now live.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+# dummy data for blogs...
+10.times do |blog_num|
+	Blog.create!(
+		title: "Blog post #{blog_num}",
+		body: "Lorem ipsum tu ne cede nalis exquisite corpse dolor"
+		)
+end
+
+
+# dummy data for skills
+5.times do |skill_num|
+	Skill.create!(
+		title: "Rails #{skill_num}",
+		percent_used: 10
+		)
+end
+
+
+# dummy data for works
+9.times do |work_num|
+	Work.create!(
+		title: "Portfolio title: #{work_num}",
+		subtitle: "my great service number #{work_num}",
+		body: "Lorem ipsum tu ne cede nalis exquisite corpse dolor",
+		main_img: "http://placehold.it/600",
+		thumb_img: "http://placehold.it/350x250"
+		)
+end
