aca2bbe36aaf75c8bfac5e0aec75ab4b380696e4
james w hubert <jwhubert91@gmail.com>
2019-11-18 15:33:56 -0500

    added custom routes for contact and about pages, and a custom show route for our work#show action

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  resources :works
-  get 'pages/home'
-  get 'pages/about'
-  get 'pages/contact'
+  resources :works, except: [:show]
+  get 'work/:id', to: 'works#show', as: 'work_show'
+
+  root to: 'pages#home'
+  get 'about', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
+
   resources :blogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
