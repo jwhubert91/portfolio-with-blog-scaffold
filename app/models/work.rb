f0a62cf7a7eca46a017e69a37cd3c5f6a7df2ef3
james w hubert <jwhubert91@gmail.com>
2019-11-20 11:46:17 -0500

    implemented data defaults in work model

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,10 +1,17 @@
 class Work < ApplicationRecord
 
+	after_initialize :set_defaults
+
 	validates :title, :body, :main_img, :thumb_img, presence: true
 	validates :body, length: {maximum: 1000}
 
 	def self.angular
 		where(subtitle: 'Angular')
+	end
+
+	def set_defaults
+		self.main_img ||= "http://placehold.it/600x400"
+		self.thumb_img ||= "http://placehold.it/350x250"
 	end
 
 end
